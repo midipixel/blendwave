3b5dd5561b59cd8fb91253d24199a66aa783c57b
Werther Azevedo <werther@midipixel.com>
2017-05-24 18:42:48 -0300

    Pequena faxina removendo arquivos inúteis, WIP da conversão da página de efeitos para PHP

--- fx.php
+++ fx.php
@@ -2,11 +2,54 @@
     <?php include('fileHeader.php')?>
 
     <?php
-        $effects = ["reverb", "delay"];
-     ?>
+        $effects = [
+            "Reverb" => [
+                "description" => "reverbera o som",
+                "params" => [
+                    "param1" => 0,
+                    "param2" => 0
+                ]
+            ],
+            "Delay" => [
+                "description" => "promove atraso do som",
+                "params" => [
+                    "param1" => 0,
+                    "param2" => 0
+                ]
+            ],
+            "Distortion" => [
+                "description" => "promove atraso do som",
+                "params" => [
+                    "param1" => 0,
+                    "param2" => 0
+                ]
+            ],
+            "Flanger" => [
+                "description" => "promove atraso do som",
+                "params" => [
+                    "param1" => 0,
+                    "param2" => 0
+                ]
+            ],
+            "Ring Modulator" => [
+                "description" => "promove atraso do som",
+                "params" => [
+                    "param1" => 0,
+                    "param2" => 0
+                ]
+            ],
+            "Compressor" => [
+                "description" => "promove atraso do som",
+                "params" => [
+                    "param1" => 0,
+                    "param2" => 0
+                ]
+            ]
+        ];
+    ?>
 
     <h3>Efeitos <em>Experimente com seu som utilizando efeitos DSP!</em></h3>
-
+	
     <div class="row">
         <form action="" autocomplete="off">
             <div class="col-sm-6">
@@ -19,43 +62,75 @@
                         <input type="radio" id="fx1_off">Desliga</input>                    
                     </label>                    
                 </h5>
-
+				
                 <div class="dspType fxType">
-                    <select id="fxSelect"> 
-                        <% for(var effect in fx){ %>                        
-                            <option value="<%= effect %>" id="fx1"><%= fx[effect].name %></option>
-                        <% } %>
+                    <select id="fxSelect">
+                    <?php
+                        // Populates combo box with effects
+                        foreach ($effects as $fx => $props) {
+                            $value = strtolower(str_replace(' ', '_', $fx));
+                            echo "<option value='{$value}'>{$fx}</option>";
+                        }
+                    ?>
                     </select>
-                    
-                    <% /*Render descriptions from _harp.json, making them available to JS*/ %>
-                    <!--script>
-                        
-                        var fxDescriptions = {
-                        <% for(var effect in fx){ %>
-                            <%= effect %> : "<%= fx[effect].description %>",
-                        <% } %>    
-                        };
-                    </script-->                    
-                    <p></p>
+
+                    <dl>
+                    <?php
+                        // Populates definition list with effects and descriptions
+                        foreach ($effects as $fx => $props) {
+                            $id = strtolower(str_replace(' ', '_', $fx));
+
+                            echo "<dt>{$fx}</dt>";
+
+                            foreach ($props as $prop => $value){
+                                if ($prop == 'description'){
+                                    echo "<dd id='{$id}'>{$value}</dd>";
+                                }
+                                break;
+                            }
+                        }
+                    ?>
+                    </dl>
                 </div>
-                
-                <% for(var effect in fx){ %> 
-                    <div class="dspSetup fxSetup <%= effect %>" id="<%= effect %>" >
+
+                <?php foreach ($effects as $fx => $props): ?>
+                    <?php $id = strtolower(str_replace(' ', '_', $fx)); ?>
+
+                    <div class='dspSetup fxSetup' id='<?= $id ?>'>
                         <figure>
-                            <img src="img/<%= effect %>.png" alt="">
+                            <img src="img/<?= $id ?>.png" alt="">
                         </figure>
-                        
-                        <div id="fxParams">
-                            <fieldset class="audioParams">
-                                <% for(var param in fx[effect].params){ %>                          
-                                    <label for="<%= param %>"><%= fx[effect].params[param].name %></label>
-                                    <input type="range" id="<%= fx[effect].params[param] %>" min="<%= fx[effect].params[param].min %>" value="<%= fx[effect].params[param].value %>" max="<%= fx[effect].params[param].max %>" step="<%= fx[effect].params[param].step %>" data-type="audioParam">
-                                    <output for="<%= param %>"><%= fx[effect].params[param].value %></output>
-                                 <% } %>
-                            </fieldset>                                              
-                        </div>
                     </div>
-                <% } %>
+
+                    <div id="fxParams">
+                        <fieldset class="audioParams">
+                            <?php foreach ($props as $prop => $value): ?>
+                                <?php if ($prop == 'params'): ?>
+                                    aqui vai ter parametro
+
+                                <?php endif; ?>
+                            <?php endforeach; ?>
+                        </fieldset>
+
+                    </div>
+                <?php endforeach; ?>
+
+                <!--div class="dspSetup fxSetup <%= effect %>" id="<%= effect %>" >
+                    <figure>
+                        <img src="img/<%= effect %>.png" alt="">
+                    </figure>
+
+                    <div id="fxParams">
+                        <fieldset class="audioParams">
+                            <% for(var param in fx[effect].params){ %>
+                                <label for="<%= param %>"><%= fx[effect].params[param].name %></label>
+                                <input type="range" id="<%= fx[effect].params[param] %>" min="<%= fx[effect].params[param].min %>" value="<%= fx[effect].params[param].value %>" max="<%= fx[effect].params[param].max %>" step="<%= fx[effect].params[param].step %>" data-type="audioParam">
+                                <output for="<%= param %>"><%= fx[effect].params[param].value %></output>
+                             <% } %>
+                        </fieldset>
+                    </div>
+                </div-->
+
             </div>
         </form>
     </div>
