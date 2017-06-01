834f7c8e17d982f14e15e5357e495c57a57e04ae
Werther Azevedo <werther@midipixel.com>
2017-06-01 20:32:48 -0300

    Replicação de componentes para todos os efeitos Ainda não estão com os valores corretos, mas cada select já mostra os parametros corretos por efeito

--- js/data/fx_data.js
+++ js/data/fx_data.js
@@ -2,51 +2,26 @@ var effects = {
     reverb : {
         name : "reverb",
         description : "reverbera o som",
-        params : {
-            time : 0,
-            decay : 0,
-            mix : 0
-        }
     },
     delay : {
         name : "delay",
         description : "promove atraso do som",
-        params : {
-            param1 : 0,
-            param2 : 0
-        }
     },
     distortion : {
         name : "distortion",
         description : "distorce som",
-        params : {
-            param1 : 0,
-            param2 : 0
-        }
     },
     flanger : {
         name : "flanger",
         description : "bobina na cabeça",
-        params : {
-            param1 : 0,
-            param2 : 0
-        }
     },
-    ringmod : {
+    ring_modulator : {
         name : "ring_modulator",
         description : "modula o anel",
-        params : {
-            param1 : 0,
-            param2 : 0
-        }
     },
     compressor : {
         name : "compressor",
         description : "comprime loucamente",
-        params : {
-            param1 : 0,
-            param2 : 0
-        }
     }
 };
 
@@ -91,6 +66,142 @@ Vue.component('delay', {
             fxName: effects.delay.name,
             fxDescription: effects.delay.description,
             img: 'img/delay.png',
+            params: [
+                {
+                    name: 'time',
+                    min: 0,
+                    max: 1,
+                    step: 0.01,
+                    value: 1,
+                },
+                {
+                    name: 'decay',
+                    min: 0,
+                    max: 1,
+                    step: 0.01,
+                    value: 1,
+                },
+                {
+                    name: 'mix',
+                    min: 0,
+                    max: 1,
+                    step: 0.01,
+                    value: 1,
+                }
+            ]
+        }
+    }
+});
+
+Vue.component('distortion', {
+    template: '#fxTemplate',
+    data: function() {
+        return {
+            fxName: effects.distortion.name,
+            fxDescription: effects.distortion.description,
+            img: 'img/distortion.png',
+            params: [
+                {
+                    name: 'time',
+                    min: 0,
+                    max: 1,
+                    step: 0.01,
+                    value: 1,
+                },
+                {
+                    name: 'decay',
+                    min: 0,
+                    max: 1,
+                    step: 0.01,
+                    value: 1,
+                },
+                {
+                    name: 'mix',
+                    min: 0,
+                    max: 1,
+                    step: 0.01,
+                    value: 1,
+                }
+            ]
+        }
+    }
+});
+
+Vue.component('flanger', {
+    template: '#fxTemplate',
+    data: function() {
+        return {
+            fxName: effects.flanger.name,
+            fxDescription: effects.flanger.description,
+            img: 'img/flanger.png',
+            params: [
+                {
+                    name: 'time',
+                    min: 0,
+                    max: 1,
+                    step: 0.01,
+                    value: 1,
+                },
+                {
+                    name: 'decay',
+                    min: 0,
+                    max: 1,
+                    step: 0.01,
+                    value: 1,
+                },
+                {
+                    name: 'mix',
+                    min: 0,
+                    max: 1,
+                    step: 0.01,
+                    value: 1,
+                }
+            ]
+        }
+    }
+});
+
+Vue.component('ring_modulator', {
+    template: '#fxTemplate',
+    data: function() {
+        return {
+            fxName: effects.ring_modulator.name,
+            fxDescription: effects.ring_modulator.description,
+            img: 'img/ring_modulator.png',
+            params: [
+                {
+                    name: 'time',
+                    min: 0,
+                    max: 1,
+                    step: 0.01,
+                    value: 1,
+                },
+                {
+                    name: 'decay',
+                    min: 0,
+                    max: 1,
+                    step: 0.01,
+                    value: 1,
+                },
+                {
+                    name: 'mix',
+                    min: 0,
+                    max: 1,
+                    step: 0.01,
+                    value: 1,
+                }
+            ]
+        }
+    }
+});
+
+Vue.component('compressor', {
+    template: '#fxTemplate',
+    data: function() {
+        return {
+            fxName: effects.compressor.name,
+            fxDescription: effects.compressor.description,
+            img: 'img/compressor.png',
             params: [
                 {
                     name: 'time',
