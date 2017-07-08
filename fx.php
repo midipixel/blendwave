dabb469a21db988a78c74f3bf316b87e054988d2
Werther Azevedo <werther@midipixel.com>
2017-07-07 22:30:01 -0300

    FX WIP Trabalhando nos vários estados da UI

--- fx.php
+++ fx.php
@@ -18,35 +18,30 @@
                         <fieldset>
                             <legend>Efeito <?= $i ?></legend>
 
-                            <label for="<?= $fxSlot . '_on' ?>">
-                                <input type="radio" name="<?= $fxSlot . 'switch' ?>" id="<?= $fxSlot . '_on' ?>" v-on:click = 'activateFX(<?= $fxSlot ?>, true)' :checked="<?= $fxSlot . '.active' ?>">Liga</input>
-                            </label>
+                            <div class="fxControls">
+                                <button class="toggleFX" v-on:click.prevent="toggleFX(<?= $fxSlot ?>)">{{ ui.fxButtonText }}</button>
 
-                            <label for="<?= $fxSlot . '_off' ?>">
-                                <input type="radio" name="<?= $fxSlot . 'switch' ?>" id="<?= $fxSlot . '_off' ?>" v-on:click = 'activateFX(<?= $fxSlot ?>, false)' :checked="!<?= $fxSlot . '.active' ?>">Desliga</input>
-                            </label>
+                                <div class="{active: <?= $fxSlot . '.active' ?>}">
+                                    <select v-model="<?= $fxSlot . '.selected' ?>" v-on:change="setFX(<?= $fxSlot ?>, <?= $fxSlot . '.selected' ?>)" disabled="disabled">
+                                        <option selected="selected" value="none">None</option>
+                                        <template v-for="fx in fxList">
+                                            <option :value='fx.name'>{{ fx.name }}</option>"
+                                        </template>
+                                    </select>
 
-                            <div class="fxSelect">
-                                <select v-model="<?= $fxSlot . '.selected' ?>" v-on:change='setFX(<?= $fxSlot ?>, <?= $fxSlot . '.selected' ?>)'>
-                                    <option value="none">Nenhum</option>
-                                    <template v-for="fx in fxList">
-                                        <option :value='fx.name'>{{ fx.name }}</option>"
-                                    </template>
-                                </select>
-
-                                <p v-for="fx in fxList">
-                                    <span v-if="<?= $fxSlot . '.selected' ?> == fx.name">{{ fx.description }}</span>
-                                </p>
+                                    <p v-for="fx in fxList">
+                                        <span v-if="<?= $fxSlot . '.selected' ?> == fx.name">{{ fx.description }}</span>
+                                    </p>
+                                </div>
                             </div>
+
+                            <template v-for="fx in fxList">
+                                <effect :id=fx.id v-if="<?= $fxSlot . '.selected'?> == fx.name" :class=fx.id></effect>
+                            </template>
                          </fieldset>
                     </form>
                 </div>
             </div>
-
-            <template v-for="fx in fxList">
-                <effect :id=fx.id v-if="<?= $fxSlot . '.selected'?> == fx.name" :class=fx.id></effect>
-            </template>
-
         </section>
     <?php endfor; ?>
 </section>
--- js/fx.js
+++ js/fx.js
@@ -22,40 +22,52 @@ fxPanel = new Vue({
         fxSlot1: {
             active: false,
             selected: 'none',
-            pizEffect: undefined
+            pizEffect: {}
         },
         fxSlot2: {
             active: false,
             selected: 'none',
-            pizEffect: undefined
+            pizEffect: {}
+        },
+        ui: {
+            fxButtonText: 'off'
         }
     },
     methods: {
-        setFX: function(fxSlot, effect){
+        toggleFX: function(fxSlot){
+            // Toggle Active State and Button Text
+            fxSlot.active = !fxSlot.active;
 
+            // Change UI according to the effect being active or not
+            if(fxSlot.active) {
+                this.ui.fxButtonText = 'on'
+                document.querySelector('.fxControls select').removeAttribute('disabled');
+            }
+            else {
+                this.ui.fxButtonText = 'off';
+                document.querySelector('.fxControls select').setAttribute('disabled', 'disabled');
+            }
+
+            // If an Effect is selected, toggle the mix value
+            if(fxSlot.selected != 'none'){
+                fxSlot.active ? fxSlot.pizEffect.mix = 1 : fxSlot.pizEffect.mix = 0;
+            }
+
+
+        },
+        setFX: function(fxSlot, effect){
              //If there is a Pizzicato effect applied, remove it
-            if(fxSlot.pizEffect != undefined){
+            if(fxSlot.pizEffect.outputNode){
                 patch.sound.removeEffect(fxSlot.pizEffect);
             }
 
             if (effect != 'none'){
                 //Create a new effect from user selection, default mix to 0
                 fxSlot.pizEffect = new Pizzicato.Effects[effect]();
-                fxSlot.pizEffect.mix = 0;
 
                 //Apply effect to sound
                 patch.sound.addEffect(fxSlot.pizEffect);
             }
-        },
-        activateFX: function(fxSlot, isActive){
-            fxSlot.active = isActive;
-
-            if(fxSlot.pizEffect != undefined){
-                isActive ? fxSlot.pizEffect.mix = 1 : fxSlot.pizEffect.mix = 0;
-            }
-
-            console.log(fxSlot.active);
-            console.log(fxSlot.pizEffect.mix);
         }
     }
 });
