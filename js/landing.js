5e6251bf0025852b2be24a34f73bcd447c138a8b
Werther <werther@midipixel.com>
2017-12-21 16:26:47 -0200

    Landing Page temporária

---
+++ js/landing.js
@@ -0,0 +1,14 @@
+/*var landing = new Vue({
+    el: '#landing',
+    data: {
+
+    },
+    methods: {
+
+    },
+    mounted: function(){
+
+    }
+});*/
+
+halkaBox.run('gallery');
---
+++ landing.php
@@ -0,0 +1,49 @@
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+    <title>Blendwave</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" type="image/png" href="img/favicon.png" />
+
+    <link rel="stylesheet" type="text/css" href="css/vendor/bootstrap-reboot.min.css">
+    <link rel="stylesheet" type="text/css" href="css/vendor/bootstrap-grid.min.css">
+    <link rel="stylesheet" type="text/css" href="css/vendor/halkaBox.min.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="css/landing.css">
+</head>
+
+<body>
+    <main class="container landingPage" id="landing">
+        <article class="row">
+            <section class="col">
+                <img src="img/bw_logo.svg" alt="Blendwave Logo" class="logo">
+                <p>Blendwave is an online sound effects maker which is currently being beta-tested. Read more about it <a href="https://repositorioaberto.uab.pt/bitstream/10400.2/6653/1/Blendwave.pdf" target="_blank">here</a>.</p>
+                <p>Want to be a beta tester? Send a message to beta [at] blendwave.net</p>
+            </section>
+        </article>
+
+        <figure class="row screenshots">
+            <div class="col-md">
+                <a href="img/ss_wave.png" class="gallery" title="Sample Panel"><img src="img/ss_wave.png" alt="Sample Panel"></a>
+            </div>
+            <div class="col-md">
+                <a href="img/ss_envelope.png" class="gallery" title="Envelope Panel"><img src="img/ss_envelope.png" alt="Envelope Panel"></a>
+            </div>
+            <div class="col-md">
+                <a href="img/ss_filter.png" class="gallery" title="Filter Panel"><img src="img/ss_filter.png" alt="Filter Panel"></a>
+            </div>
+            <div class="col-md">
+                <a href="img/ss_fx.png" class="gallery" title="Effects Panel"><img src="img/ss_fx.png" alt="Effects Panel"></a>
+            </div>
+            <div class="col-md">
+                <a href="img/ss_output.png" class="gallery" title="Output Panel"><img src="img/ss_output.png" alt="Output Panel"></a>
+            </div>
+        </figure>
+    </main>
+    <script src="js/vendor/halkaBox.min.js"></script>
+    <script src="js/landing.js"></script>
+</body>
+</html>
